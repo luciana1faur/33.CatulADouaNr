49623ddc7842ccc0f6a4ae9f1a44984a103a15f7
luciana1faur <luciana_faur@yahoo.com>
2020-09-26 14:35:01 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cout << "Introduceti deimpartitul si impartitorul (despartite prin spatiu): ";
+  cin >> a >> b;
+
+  c = a / b;
+
+  cout << "Catul dintre " << a << " si " << b << " este " << c << endl;
+
+  return 0;
+}
+//Write a program in C++ to divide two numbers and print on the screen. 
